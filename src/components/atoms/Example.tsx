cdedf7354e5cb81bb5d3a6596ee8894c47c16e93
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-03-26 23:37:53 +0900

    :wrench: chore: テスト整備

--- src/components/atoms/Example.tsx
+++ src/components/atoms/Example.tsx
@@ -1,6 +1,5 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 
 export const Example: FC = () => {
-  const [isShow, setIsShow] = useState(false);
   return <div>Example</div>;
 };
